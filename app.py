5e2aa44e74a08a23375a7525f51f0ce556b23c80
Anmol Goyal <anmolgoyal@gmail.com>
2020-10-03 19:07:17 +0530

    updated routes for LandingPage and 404 pages

--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__, static_url_path='/static')
 
+app.debug = True 
+app.secret_key = "NothingIsSecret"
+
 
 @app.route('/')
 def HomePage():
-   return "Allocative HomePage"
+   return render_template("index.html")
+
+
+@app.errorhandler(404)
+def error404(error):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
